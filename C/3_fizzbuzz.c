1be84d94671feede5331f19e3ecc9de729a4125b
ane <118138658+anesth3siaaa@users.noreply.github.com>
2026-03-09 11:57:50 +0100

    Implement FizzBuzz program in C

---
+++ C/3_fizzbuzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+int i;
+for (i = 0; i <= 100; i++) {
+    if (i == 0) {
+        printf("This number doesn't count.\n");
+    } else if (i % 15 == 0) {
+        printf("FizzBuzz\n");
+    } else if (i % 3 == 0) {
+        printf("Fizz\n");
+    } else if (i % 5 == 0) {
+        printf("Buzz\n");
+    } else {
+        printf("%d\n", i);
+    }
+}
+    return 0;
+}
