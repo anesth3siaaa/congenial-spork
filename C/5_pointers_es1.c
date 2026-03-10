685c73ce872aa10e258c00d78ec92b0fcb55afe8
ane <118138658+anesth3siaaa@users.noreply.github.com>
2026-03-10 08:58:13 +0100

    Add swap function and demonstrate in main
    Implement a swap function to exchange values of two integers using pointers.

---
+++ C/5_pointers_es1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void swap(int *p1, int *p2) { 
+    int temp = *p1; // temp grabs the value of *p1 (6)
+    *p1 = *p2;      // *p1 now grabs the value of *p2 (7)
+    *p2 = temp;     // *p2 now grabs the value of temp (6, which grabbed from *p1)
+}
+/* By giving the swap function these parameters I'm telling it what the 
+    pointers are pointing towards when the values I want it to swap
+    are declared inside of the swap call of main */
+
+
+int main()
+{
+    
+    int a = 6;
+    int b = 7;
+    printf("Before swap: %d, %d\n", a, b);
+    swap(&a, &b); /* This tells swap that *p1 = a and *p2 = b */
+    printf("After swap: %d, %d\n", a, b);
+
+    return 0;
+}
