05aac3221851b474db744a8326fb8fe9eb1b0eae
ane <118138658+anesth3siaaa@users.noreply.github.com>
2026-05-11 11:01:45 +0200

    Add career guidance function for developers

---
+++ Python/4_development_career_helper.py
@@ -0,0 +1,50 @@
+def listCode():
+    while True:
+        print("What do you want to learn?")
+        print("""    1. Web development
+    2. Game development
+    3. Data science
+    4. Mobile development
+    5. Systems programming
+    6. Cybersecurity""")
+        answer = input()
+        
+        answers = ["1", "2", "3", "4", "5", "6"]
+        
+        if answer in answers:
+            if answer == answers[0]:
+                print("Learn Javascript, HTML, CSS.")
+            elif answer == answers[1]:
+                print("What engine inspires you the most?")
+                print("""    1. Unity
+    2. Unreal Engine
+    3. Godot
+    4. I want to use a framework""")
+                engine = input()
+                if engine == "1":
+                    print("Learn C#.")
+                elif engine == "2":
+                    print("Learn C++.")
+                elif engine == "3":
+                    print("Learn GDScript or C#.")
+                else:
+                    print("Learn LOVE2D it is stupidly good.")
+            elif answer == answers[2]:
+                print("Learn Python or R.")
+            elif answer == answers[3]:
+                print("iOS only?")
+                iosOnly = input("[y/N]: ")
+                
+                
+                if iosOnly == "y":
+                    print("Learn Swift.")
+                else:
+                    print("Learn React.")
+            elif answer == answers[4]:
+                print("C for systems, C++ for Arduino.")
+            elif answer == answers[5]:
+                print("Learn how to use Kali.")
+            else:
+                print("You should choose something.")
+                continue
+            break
