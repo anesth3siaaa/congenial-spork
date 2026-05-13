d2bb74e74253064a05cbdb874b85fffb4c1a2986
ane <118138658+anesth3siaaa@users.noreply.github.com>
2026-05-13 14:37:51 +0200

    Refactor learning options input handling

--- Python/4_development_career_helper.py
+++ Python/4_development_career_helper.py
@@ -1,16 +1,17 @@
 def listCode():
-    while True:
-        print("What do you want to learn?")
-        print("""    1. Web development
+    answers = ["1", "2", "3", "4", "5", "6"]
+    print("What do you want to learn?")
+    print("""    1. Web development
     2. Game development
     3. Data science
     4. Mobile development
     5. Systems programming
     6. Cybersecurity""")
+    while True:
         answer = input()
-        
-        answers = ["1", "2", "3", "4", "5", "6"]
-        
+
+
+
         if answer in answers:
             if answer == answers[0]:
                 print("Learn Javascript, HTML, CSS.")
@@ -21,19 +22,25 @@ def listCode():
     3. Godot
     4. I want to use a framework""")
                 engine = input()
-                if engine == "1":
-                    print("Learn C#.")
-                elif engine == "2":
-                    print("Learn C++.")
-                elif engine == "3":
-                    print("Learn GDScript or C#.")
-                else:
-                    print("Learn LOVE2D it is stupidly good.")
+                while True:
+                    if engine == "1":
+                        print("Learn C#.")
+                    elif engine == "2":
+                        print("Learn C++.")
+                    elif engine == "3":
+                        print("Learn GDScript or C#.")
+                    elif engine == "4":
+                        print("Learn LOVE2D it is stupidly good.")
+                    else:
+                        print("You should choose something.")
+                        engine = input()
+                        continue
+                    break
             elif answer == answers[2]:
                 print("Learn Python or R.")
             elif answer == answers[3]:
                 print("iOS only?")
-                iosOnly = input("[y/N]: ")
+                iosOnly = input("[y/N]: ").casefold()
                 
                 
                 if iosOnly == "y":
@@ -44,7 +51,7 @@ def listCode():
                 print("C for systems, C++ for Arduino.")
             elif answer == answers[5]:
                 print("Learn how to use Kali.")
-            else:
-                print("You should choose something.")
-                continue
-            break
+        else:
+            print("You should choose something.")
+            continue
+        break
